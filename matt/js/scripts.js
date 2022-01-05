1b4cbc7c4301f479046d08a61f0166d638dd3b31
Matt Pharr <matt@pharr.org>
2022-01-05 10:03:33 -0800

    update

--- matt/js/scripts.js
+++ matt/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
     * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2021 Start Bootstrap
+    * Copyright 2013-2022 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
     (function ($) {
